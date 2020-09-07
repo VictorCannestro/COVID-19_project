cab993126456169b2297356207d51a81b9435af1
VictorCannestro <vcannestro@gmail.com>
2020-09-06 21:38:56 -0700

    Add bokeh plot

--- tests/test_helper_functions.py
+++ tests/test_helper_functions.py
@@ -7,6 +7,21 @@ from helper_functions import sumByDate
 test_data = pd.DataFrame({v for v in range(1,11)}, index=range(10))
 
 class TestDailyChanges(object):
+    '''
+    Here are several assert methods for reference:
+            assertEqual(a, b)           a == b
+            assertNotEqual(a, b)        a != b
+            assertTrue(x)              bool(x) is True
+            assertFalse(x)              bool(x) is False
+            assertIs(a, b)              a is b
+            assertIsNot(a, b)           a is not b
+            assertIsNone(x)             x is None
+            assertIsNotNone(x)          x is not None
+            assertIn(a, b)              a in b
+            assertNotIn(a, b)           a not in b
+            assertIsInstance(a, b)      isinstance(a, b)
+            assertNotIsInstance(a, b)   !isinstance(a, b)
+    '''
     def test_shape(self):
         assert True
         
