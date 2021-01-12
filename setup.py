7efc27be7756850e282bee823212f3e3259af7ea
Victor C Cannestro <vcannestro@gmail.com>
2021-01-11 18:52:47 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='Covid-19 Project',
+    author='Victor Cannestro',
+    description='Visualizations of the 2019-2021 COVID-19 pandemic in the US',
+    packages=['COVID-19_project']
+)
